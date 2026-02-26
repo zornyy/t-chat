6509ecf0d24f94d4dea312518bdb04a61578424d
Zorn Alexandre <alexandre.zorn2005@gmail.com>
2026-02-26 15:10:45 +0100

    feat - basic interface layout

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"go-chat/display"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 func main() {
-	display.StartChatUI()
 
 	// var i int
 
@@ -17,4 +17,43 @@ func main() {
 	// } else {
 	// 	client.Start()
 	// }
+
+	app := tview.NewApplication()
+
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(text)
+	}
+
+	inputField := tview.NewInputField().
+		SetLabel("-> ").
+		SetFieldWidth(0).
+		SetFieldTextColor(tcell.ColorBlack).
+		SetFieldBackgroundColor(tcell.ColorWhite).
+		SetDoneFunc(func(key tcell.Key) {
+			app.Stop()
+		})
+
+	msgIput := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(inputField, 0, 1, true)
+
+	sideBar := newPrimitive("Current connection")
+	messagesContainer := newPrimitive("Messages will show up here.")
+
+	grid := tview.NewGrid().
+		SetRows(3, 0, 3).
+		SetColumns(30, 100).
+		SetBorders(true).
+		AddItem(newPrimitive("Welcome to T-CHAT ! A TCP Chat app running in the terminal !"), 0, 0, 1, 3, 0, 0, false).
+		AddItem(msgIput, 2, 0, 1, 3, 0, 0, false)
+
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	grid.AddItem(sideBar, 1, 0, 1, 1, 1, 0, false).
+		AddItem(messagesContainer, 1, 0, 1, 3, 0, 0, false)
+
+	if err := tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
+		panic(err)
+	}
 }
